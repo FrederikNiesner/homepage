42a91fb6e99f9fcf0f19a567575c673bc9fb8b2c
amorbass <frederik.nieser@gmail.com>
2017-08-14 17:44:57 +0200

    Updated all soundcloud pages

--- js/creative.js
+++ js/creative.js
@@ -3,10 +3,10 @@
 
     // jQuery for page scrolling feature - requires jQuery Easing plugin
     $(document).on('click', 'a.page-scroll', function(event) {
-      var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: ($($anchor.attr('href')).offset().top - 50)
-        }, 1250, 'easeInOutExpo');
+        var $anchor = $(this);
+          $('html, body').stop().animate({
+              scrollTop: ($($anchor.attr('href')).offset().top - 50)
+            }, 1250, 'easeInOutExpo');
         event.preventDefault();
     });
 
